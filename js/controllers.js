4ce6625176f782a59713fdfdbc547ae87caa1347
raaum <greg.raaum@yahoo.com>
2016-05-02 19:40:28 -0500

    http service added for json GET.

--- js/controllers.js
+++ js/controllers.js
@@ -1,56 +1,7 @@
 var roloDexter = angular.module('roloDexter', []);
 
-roloDexter.controller('roloCtrl', function roloCtrl($scope) {
-	$scope.contacts = [
-	{
-		"name": "Bart Simpson",
-		"organization": "None",
-		"email": "bart@simpson.bart",
-		"mobile": "555-555-5555",
-		"facebook": "bartsimpson",
-		"twitter": "therealbart",
-		"notes": "The sky is blue and Bart is yellow.",
-		"photo": "photos/Bart_Simpson.jpg"
-	},
-	{
-		"name": "Mark Vlatsmith",
-		"organization": "Rolling Thunder Film School",
-		"email": "mark@mkvlat.com",
-		"mobile": "555-555-5555",
-		"facebook": "markvlatsmith",
-		"twitter": "besmartmark",
-		"notes": "Best friends forever.",
-		"photo": "photos/Mark_Vlatsmith.jpg"
-	},
-	{
-		"name": "Johnny Sledgesickle",
-		"organization": "Tasty Frozen Treats",
-		"email": "john@tftreats.com",
-		"mobile": "555-555-5555",
-		"facebook": "thesledge",
-		"twitter": "jbsledge",
-		"notes": "Makes a great snowcone.",
-		"photo": "photos/Johnny_Sledgesickle.jpg"
-	},
-	{
-		"name": "Barbara Waltshizzler",
-		"organization": "Nogokano Limited",
-		"email": "barb@nogokanoltd.com",
-		"mobile": "555-555-5555",
-		"facebook": "waltshizz",
-		"twitter": "candobarb",
-		"notes": "Former nanny.  Plays rugby.",
-		"photo": "photos/Barbara_Waltshizzler.jpg"
-	},
-	{
-		"name": "Grandpa Palmer",
-		"organization": "Retired",
-		"email": "nate@grandpalmer.com",
-		"mobile": "555-555-5555",
-		"facebook": "grampz",
-		"twitter": "callmegrampz",
-		"notes": "Grandpa.  I think he's still alive.",
-		"photo": "photos/Grandpa_Palmer.jpg"
-	}
-	]
+roloDexter.controller('roloCtrl', function roloCtrl($scope, $http) {
+	$scope.data = $http.get('json/contacts.json').success(function(data) {
+		$scope.contacts = data;
+	});
 });
