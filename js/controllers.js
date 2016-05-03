0cfaa14ae59e7b171c1332e3a22700e2947efbd8
raaum <greg.raaum@yahoo.com>
2016-05-02 20:41:23 -0500

    integrating search function

--- js/controllers.js
+++ js/controllers.js
@@ -2,8 +2,10 @@ var roloDexter = angular.module('roloDexter', []);
 
 roloDexter.controller('roloCtrl', ['$scope', '$http',
 	function($scope, $http) {
-		$scope.data = $http.get('json/contacts.json').success(function(data) {
-			$scope.contacts = data;
-		});
+		$scope.data = $http.get('json/contacts.json')
+			.success(function(data) {
+				$scope.contacts = data;
+			}
+		);
 	}
 ]);
