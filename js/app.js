134826bad758f52b81ace7111f572fac239f5e00
raaum <greg.raaum@yahoo.com>
2016-05-10 08:43:10 -0500

    added routes, app.js, dependencies, partials

--- js/app.js
+++ js/app.js
@@ -1,2 +1,16 @@
-/* app.js */
+var roloDexter = angular.module('roloDexter', ['ngRoute','contactControllers']);
 
+roloDexter.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+    when('/list', {
+        templateUrl: 'partials/list.html',
+        controller: 'ListController'
+    }).
+    when('/details/:itemId', {
+        templateUrl: 'partials/details.html',
+        controller: 'DetailsController'
+    }).
+    otherwise({
+        redirectTo: '/list'
+    });
+}]);
--- js/controllers.js
+++ js/controllers.js
@@ -1,11 +1,29 @@
-var roloDexter = angular.module('roloDexter', []);
+var contactControllers = angular.module('contactControllers', ['ngAnimate']);
 
-roloDexter.controller('roloCtrl', ['$scope', '$http',
-	function($scope, $http) {
-		$scope.data = $http.get('json/contacts.json')
-			.success(function(data) {
-				$scope.contacts = data;
-			}
-		);
-	}
-]);
+contactControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
+    $http.get('json/contacts.json').success(function(data) {
+        $scope.contacts = data;
+        $scope.orderField = 'name';
+    });
+}]);
+
+contactControllers.controller('DetailsController', ['$scope', '$http','$routeParams', function($scope, $http, $routeParams) {
+    $http.get('js/contacts.json').success(function(data) {
+        $scope.contacts = data;
+        $scope.whichItem = $routeParams.itemId;
+
+        if ($routeParams.itemId > 0) {
+            $scope.prevItem = Number($routeParams.itemId)-1;
+        }
+        else {
+            $scope.prevItem = $scope.contacts.length-1;
+        }
+
+        if ($routeParams.itemId < $scope.contacts.length-1) {
+            $scope.nextItem = Number($routeParams.itemId)+1;
+        }
+        else {
+            $scope.nextItem = 0;
+        }
+    });
+}]);
